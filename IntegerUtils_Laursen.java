d14cc9e6fced75e507360062c322702884727de8
Julie Laursen <julielaursen1@gmail.com>
2019-02-06 22:27:47 -0600

    "second to last draft"

--- IntegerUtils_Laursen.java
+++ IntegerUtils_Laursen.java
@@ -14,78 +14,326 @@ Test middle of the road, corner cases, and extreme cases
 
 package utils;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public interface IntegerUtils_Laursen {
 	
-	int k = 0;
-	int m = 0;
-	int n = 0;
-	int maximum = 0;
-	int h_q_3 = 0;
-	int h_r_5 = 0;
-	int[] intArray = new int[10];
+
+	public static void main(String[] a){ 
+		int k = 98;
+		long kk= 8;
+		int l = 0;
+		int p = -2;
+		int q = 1;
+		int r = -1;
+		int s = 5;
+		int t = 2;
+		int m = 9;
+		int n = 12;
+		int o = 4;
+		int[] intArray = {39, 41, 41, 45};
+		int[] intArray2 = {41};
+		int[] intArray3 = {2, 2, 2, 2};
+		int target = 41;
+		int b = 7;
+		int c = 50;
+		int d = 10;
+		int firstnum = 9;
+		int secondnum = 4;
+		Integer arr[] = {2,4,6,8,10};
+		Set<Integer> set = new HashSet<>(Arrays.asList(arr));
+		Integer arr2[] = {2};
+		Set<Integer> set2 = new HashSet<>(Arrays.asList(arr2));
+		Integer arr3[] = {2,2,2};
+		Set<Integer> set3 = new HashSet<>(Arrays.asList(arr3));
+		
+		isEven(k);
+		isEven(l);
+		isEven(p);
+		isOdd(s);
+		isOdd(q);
+		isOdd(r);
+		isPrime(s);
+		isPrime(t);
+		isPrime(q);
+		greatestCommonFactor(m, n);
+		greatestCommonFactor(q, r);
+		greatestCommonFactor(l, l);
+		getGreatestConstrainedMultiple(b, c);
+		getGreatestConstrainedMultiple(t, o);
+		getGreatestConstrainedMultiple(l, q);
+		isSorted(intArray);
+		isSorted(intArray2);
+		reverse(k);
+		reverse(s);
+		reverse(d);
+		getMinimum(intArray);
+		getMinimum(intArray2);
+		getMinimum(intArray3);
+		getMaximum(set);
+		getMaximum(set2);
+		getMaximum(set3);
+		getSmallestIndexofMatch(intArray, target); 
+		getSmallestIndexofMatch(intArray2, target);
+		getIntegerH(firstnum, secondnum);
+		getSumthing(kk);
+    }
 	
+    Scanner console = new Scanner(System.in);
+ 
 	public static boolean isEven(int k) {
+		System.out.println("The results of the 'isEven' method: ");
+		if (k % 2 == 0 && k != 0 && k > 0) {
+			System.out.println("Number " + k + " = Middle of the road case");
+			return true;
+		}
+		if (k == 0) {
+			System.out.println("Number " + k + " = Corner case");
+			return true;
+		}
+		if (k % 2 == 0 && k < 0) {
+			System.out.println("Number " + k + " = Degenerate case");
+			return true;
+		} else {
+			System.out.println("The number entered was not even");
+		}
 		return false;
 	}
-	
+
 	public static boolean isOdd(int k) {
+		System.out.println("The results of the 'isOdd' method: ");
+		if (k % 2 != 0 && k != 1 && k > 0) {
+			System.out.println("Number " + k + " = Middle of the road case");
+			return true;
+		}
+		if (k == 1) {
+			System.out.println("Number " + k + " = Corner case");
+			return true;
+		}
+		if (k % 2 != 0 && k < 0) {
+			System.out.println("Number " + k + " = Degenerate case");
+			return true;
+		} else {
+			System.out.println("The number entered was not odd");
+		}
+		return false;
+	}
+
+	public static boolean isPrime(int k) {
+		System.out.println("The results of the 'isPrime' method: ");
+		for (int i = 2; i < k; i++) {
+			if (k % i == 0) {
+				System.out.println("Number " + k + " = The number entered was not prime");
+				return false;
+			}
+			if (k != 2 && k != 1) {
+				System.out.println("Number " + k + " = Middle of the road");
+				return true;
+			}
+		}
+		if (k == 2) {
+			System.out.println("Number " + k + " = Corner case");
+			return true;
+		}
+		if (k == 1) {
+			System.out.println("Number " + k + " = Degenerate case");
+			return true;
+		}
+		if (k == 0) {
+			System.out.println("The number entered was not prime");
+			return false;
+		}
 		return false;
 	}
 	
-	public static boolean isPrime(int k)
-	{
-		return false;
+	public static int greatestCommonFactor(int n, int m) {
+		System.out.println("Results of the 'greatestCommonFactor' method: ");
+        int gcd = 0;
+        
+        if ((m == -1 && n == 1) || (m == 1 && n == -1)) {
+        	gcd = 1;
+        	System.out.println("Number " + gcd + " Corner case");
+        }
+        else if (m == 0 && n == 0) {
+        	System.out.println("Number " + gcd + " Degenerate case");
+        }
+        else if (n > m) {
+            gcd = n % m;
+            System.out.println("Number " + gcd + " Middle of the road");
+        }
+        else if (m > n) {
+        	gcd = m % n;
+            System.out.println("Number " + gcd + " Middle of the road");
+        }       
+        return gcd;
 	}
 	
-	public static int greatestCommonFactor(int m, int n)
-	{
-		throw new RuntimeException("Not implemented!");
-	}
-	
-	public static int getGreatestConstraintedMultiple(int k, int maximum)
-	{
-		throw new RuntimeException("Not implemented!");
-	}
-	
-	public static int getIntegerH(int h_q_3, int h_r_5)
-	{
-		throw new RuntimeException("Not implemented!");
-	}
-	
-	public static int getMaximum(Set<Integer> integerSet)
-	{
-		throw new RuntimeException("Not implemented!");
-	}
-	
-	public static int getMinimum(int[] intArray)
-	{
-		throw new RuntimeException("not implemented!");
-	}
-	
-	public static boolean isSorted(int[] intArray)
-	{
-		throw new RuntimeException("not implemented!");
-	}
-	
-	public static int getSmallestIndexofMatch(int[] intArray, int target)
-	{
-		throw new RuntimeException("not implemented!");
-	}
-	
-	public static int reverse(int k)
-	{
-		throw new RuntimeException("not implemented!");
-	}
-	
-	public static int sumthing(long k)
-	{
-		throw new RuntimeException("not implemented!");
-	}
+	public static int getGreatestConstrainedMultiple(int n, int m) {
+		System.out.println("The result of the greatest constrainted multiple method: ");
+		int counter = 0;
+		for(int i = 0; i < m; i++) {
+			if ((n * i) < m) {
+				//store n * i in a temp variable
+				counter = n * i;
+			}
+		}
+		System.out.println("Middle of the road case: " + counter);
+		if (m == 2 && n == 4) {
+			System.out.println("Corner case");
+		}
+		if (m == 0 && n == 1) {
+			System.out.println("Degenerate case");
+		}
+		return 0;
 
-
-
+	}
+	
+	public static int isSorted(int []array) {
+		System.out.println("Results of the 'isSorted' method: ");
+		int temp;
+		if (Array.getLength(array) == 1) {
+			System.out.println("Array of length one, Corner Case");
+		}
+		else {
+		for (int i = 1; i < array.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (array[j] < array [j - 1]) {
+					temp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = temp;
+				}
+			}
+		}
+			System.out.println("Middle of the road case");
+		}
+		
+		return 0;
+	} 
 	
 
+	public static int reverse(int k) {
+		System.out.println("Results of the reverse method: ");
+		int reversedNum = 0;
+	      //System.out.println("Enter an integer to reverse");
+	      //Scanner in = new Scanner(System.in);
+	      //k = in.nextInt();
+	      if(k != 0)
+	      {
+	    	  if (k == 10) {
+	  			System.out.println("Corner case");
+	  			return k;
+	  		}
+	    	  if (k > 0 && k < 10) {
+	    		  System.out.println("One digit. Degenerate case");
+	    		  return k;
+	    	  }
+	    	  else {
+	    		  //this code is wrong, needs to be redone
+	    		    for (int i = k; i !=0; i /= 10) {
+	    		    	reversedNum = reversedNum * 10 + i % 10;
+	    		    }
+	          System.out.println(reversedNum);
+	  		  return reversedNum;
+	    	  }
+	      }
+		return reversedNum;
+	}
+	
+	
+	public static int getMinimum(int []array) {
+		System.out.println("Results of the 'getMinimum' method: ");
+		//corner case would be one integer in the array
+		int min = array[0];
+		if(array.length == 1) {
+			System.out.println("One index array, Corner Case");
+			return min;
+		}
+		for (int i=0; i < array.length; i++) {
+			if(array[i] < min) {
+				min = array[i];
+				System.out.println(min);
+			}
+		}
+		System.out.println("Number " + min + " = Middle of the road");
+		//includes minimum where an array looks like {2, 2, 2, 2}
+		return min;
+	}
+	
+	public static int getIntegerH(int x, int y) {
+		System.out.println("The result of the getIntegerH method:");
+		int n = x * 4;  //36
+		int m = x * 3; //27
+		for(int i = m; i < n; i++) {
+				if(i % 5 == y && i / 3 == x) {
+					System.out.println(i);
+				}		
+		}
+		//integer H example: (9, 4) = 29
+		//(20, 1) = 61
+		//(0,0) = 0
+		//the first digit is the number that 29 can be divided int
+		//Math.max(arg0, arg1)
+		//corner case would be one integer in the array
+		//example: ({2,4,6,8,10}) = 10
+		//example: (1, 17, 22, 48, 19) = 48
+		return 0;
+	}
+	
+	public static int getMaximum(Set<Integer> set) {
+		System.out.println("Results of the 'getMaximum' method: ");
+		int x = 0;
+		if(set.size() == 1) {
+			System.out.println("Corner case");
+		}
+		for(int i = 0; i < set.size(); i++) {
+			x = Collections.max(set);
+		}
+		System.out.println("Item " + x + " in the set, middle of the road case");
+		return 0;
+	}
+	
+	public static int getSmallestIndexofMatch(int[] intArray, int target) {
+		System.out.println("Results of the 'getSmallestIndexOfMatch' method: ");
+		// get smallest index that matches a number
+		//ex) [2, 4, 6, 8, 10], 6 -> 2
+		//ex) [2, 2, 2, 2, 2], 2 -> 0
+ 		
+		if(intArray.length == 1) { //and number is in that array
+			System.out.println("Corner Case, length of one");
+			return target;
+		}
+		for (int i=0; i < intArray.length; i++) {
+			if(intArray[i] == target) {
+				System.out.println("Middle of the road case. The number " + intArray[i] + " is in index " + i);
+				break;
+			}
+		}
+		return target;
+	}
+	
+	public static long getSumthing(long kk) {
+		System.out.println("Result of the getSumthing method:" );
+		if(kk < 10) {
+        	System.out.println("Number " + kk + " Corner Case");
+        } 
+		while (kk > 9) {
+            long sum = 0;
+        	sum = sum + kk % 10; //9
+            kk = kk / 10; //2
+            kk = sum + kk;
+        }
+		 
+        System.out.println("Middle of the road case: " + kk);
+        return kk;
+      	}
 }
+
+
+	
+
+
